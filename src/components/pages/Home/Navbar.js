cec9c23ddaffd8f4eb4df0908a68a652fbbc355d
lgalvismanda <lgalvis@mnatg.com>
2021-04-15 15:12:14 -0500

    asjutes propiedades media screen

--- src/components/pages/Home/Navbar.js
+++ src/components/pages/Home/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if (window.innerWidth <= 960) {
+    if (window.innerWidth <= 961) {
       setButton(false);
     } else {
       setButton(true);
