b074a200a8cfac868bd6f164abeb7a92bebab672
Esteban1135730 <70926203+Esteban1135730@users.noreply.github.com>
2021-04-22 12:01:46 -0500

    pantalla quienes somos

--- src/components/pages/QuienesSomos/SectionA.jsx
+++ src/components/pages/QuienesSomos/SectionA.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import '../../../App.css';
 import '../../../assets/styles/Home/Home.scss'
-import '../../../assets/styles/QuienesSomos/QuienesSomos.css'
+import '../../../assets/styles/QuienesSomos/QuienesSomos.scss'
 import imagenA from '../../../assets/images/QuienesSomos/q-s-celu.png'
 import { Link } from 'react-router-dom';
+import { Grid, Paper } from '@material-ui/core';
 
 
 function SectionA() {
   return (
-<div className='homebackground sectionA-columns-quienes Rectangle-quienes'> 
-    <h1 className='QUIENES-SOMOS'>QUIENES SOMOS</h1>
-    <img className="imagenA-quienes" src={imagenA} alt="imagenA"/>
+    <div className='homebackground sectionA-columns-quienes Rectangle-quienes'>
+      <Grid container spacing={1}>
+        <Grid item xs={4}>
+        <p className='QuienesSomos'>{'QUIENES \n SOMOS'}</p>
+        <p className='TextQuienes'>Lanatax es una compañía que combina gente y tecnología para innovar los servicios profesionales que proveemos en el sector financiero. La base de nuestros principios son valores que promueven eficiencia y eficacia para asegurar la mejor calidad a nuestros clientes.
+</p>
+        </Grid>
+        <Grid item xs={2}>
+          <img className="imagenAQuienes" src={imagenA} alt="imagenA" />
+        </Grid>
+      </Grid>
     </div>
 
 
-    
+
   );
 }
 
