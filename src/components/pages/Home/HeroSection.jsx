1bbd3fffeb76e7eefe899a7ddc84e2813255e1c6
CristianPedraza <cristian@mnatg.com>
2021-04-29 09:38:54 -0500

    Grid

--- src/components/pages/Home/HeroSection.jsx
+++ src/components/pages/Home/HeroSection.jsx
@@ -1,17 +1,20 @@
+// React
 import React from 'react';
+// Components
+import { Link } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
+// Styles
 import '../../../App.scss';
-import { Button } from './Button';
 import '../../../assets/styles/Home/Home.scss'
-import text from '../../../assets/images/Home/init/text.png'
+// Assets
 import initbutton from '../../../assets/images/Home/init/boton-empezar.png'
 import storelabels from '../../../assets/images/Home/init/app-store.png'
 import storelabels2 from '../../../assets/images/Home/init/google-play.png'
-import { Link } from 'react-router-dom';
 
 
 function HeroSection() {
   return (
-    <div className='Photo'>
+    <Grid className='Photo'>
       {/*<video src='/videos/video-1.mp4' autoPlay loop muted /> */}
       <p className='no-te'>UNA MEJOR EXPERIENCIA EN:</p>
       <p className='impuestos'>IMPUESTOS</p>
@@ -23,15 +26,15 @@ function HeroSection() {
           <img className="btn-start" src={initbutton} alt="initbutton"/>
         </Link>
       </div>
-      <div className='empezar-gratis-logos'>
+      <Grid className='empezar-gratis-logos'>
         <a href='https://apps.apple.com/co/app/lanatax/id1556736650' >
           <img className="Logo-store" src={storelabels} alt="storelabels"/>
         </a>
         <a href='https://play.google.com/store/apps/details?id=com.lanatax' >
           <img className="Logo-store" src={storelabels2} alt="storelabels2"/>
         </a>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 }
 
