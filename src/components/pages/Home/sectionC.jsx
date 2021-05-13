3d552d51a0f6a2309825347b778a2d83ad04c53e
CristianPedraza <cristian@mnatg.com>
2021-05-13 14:49:19 -0500

    Replace carousel

--- src/components/pages/Home/sectionC.jsx
+++ src/components/pages/Home/sectionC.jsx
@@ -4,7 +4,7 @@ import imageOne from '../../../assets/images/Home/sectionC/NoticeOne/image.png'
 import imageTwo from '../../../assets/images/Home/sectionC/NoticeTwo/image.png'
 import imageThree from '../../../assets/images/Home/sectionC/NoticeThree/image.png'
 import { Link } from 'react-router-dom';
-import Carousel  from 'react-elastic-carousel'
+import Carousel from 'react-material-ui-carousel';
 
 function sectionC() {
   
@@ -31,15 +31,14 @@ function sectionC() {
         <h1 className='Millones-nos-Respald'>Millones nos Respaldan</h1>
         <h2 className='nase-a-especialista'>Únase a especialistas en TAXES y obtenga estrategias probadas de impuestos por correo electrónico</h2>
       </div>
-      <Carousel  itemsToShow={3} className="carrusel">
-        {items.map(item => <div key={item.id}>{item.boddy}{item.title}{item.subtitle}</div>)}
-      </Carousel >
-      <Carousel  itemsToShow={2} className="carruselTablet">
-        {items.map(item => <div key={item.id}>{item.boddy}{item.title}{item.subtitle}</div>)}
-      </Carousel >
-      <Carousel  itemsToShow={1} className="carruselMovil">
-        {items.map(item => <div key={item.id}>{item.boddy}{item.title}{item.subtitle}</div>)}
-      </Carousel >
+      <Carousel className='carrusel' autoPlay={false} navButtonsAlwaysInvisible animation='slide' > 
+        <div className='destockItems' >
+          {items.map(item => <div key={item.id}>{item.boddy}{item.title}{item.subtitle}</div>)}
+        </div>
+      </Carousel>
+      <Carousel className='carruselMovil' autoPlay={false} navButtonsAlwaysInvisible animation='slide' indicatorIconButtonProps={{style: {color: '#efefef'}}} activeIndicatorIconButtonProps={{style: {color: '#009245'}}} > 
+        {items.map(item => <div key={item.id} className='itemCarousel' >{item.boddy}{item.title}{item.subtitle}</div>)}
+      </Carousel>
     </div>
   );
 }
