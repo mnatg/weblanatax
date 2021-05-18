a62360e0bc508052b87f87bc14e0b06ee1b3c4d3
CristianPedraza <cristian@mnatg.com>
2021-05-18 15:32:05 -0500

    Update lobby

---
+++ src/components/pages/Lobby/AdviserLobby.jsx
@@ -0,0 +1,12 @@
+// React
+import React from 'react';
+
+const AdviserLobby = () => {
+    return (
+        <div style={{ marginTop: '5.5em', paddingLeft: '1em' }} >
+            
+        </div>
+    )
+}
+
+export default AdviserLobby
--- src/components/pages/Lobby/index.jsx
+++ src/components/pages/Lobby/index.jsx
@@ -3,13 +3,15 @@ import React, { useState, useEffect } from 'react';
 // Redux
 import { useSelector, useDispatch } from 'react-redux';
 import { onAddTaxes } from '../../../Store/actions/Taxes';
+// Components
+import AdviserLobby from './AdviserLobby';
 // Auth
 import { useFirestore } from 'reactfire';
 import 'firebase/firestore';
 import { useDocument } from 'react-firebase-hooks/firestore';
 // Services
-// import GetConsultancyRoomService from '../../../Services/ConsultancyRoom/GetConsultancyRoom';
-// import GetAviserService from '../../../Services/Adviser/GetAdviser';
+import GetConsultancyRoomService from '../../../Services/ConsultancyRoom/GetConsultancyRoom';
+import GetAviserService from '../../../Services/Adviser/GetAdviser';
 import ListTaxes from '../../../Services/Taxes/ListTaxes';
 // Assets
 import avatar from '../../../assets/images/Common/avatar.png';
@@ -20,6 +22,9 @@ const Lobby = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.auth.user);
     const [open, setOpen] = useState(true);
+    const [adviserPopUp, setAdviserPopUp] = useState(false);
+    const [adviser, setAdviser] = useState({});
+    const [consultancyRoom, setConsultancyRoom] = useState({});
     const [name, setName] = useState('');
     const [photoURL, setPhotoURL] = useState('');
     const userStateQuery = useFirestore().doc('userState/' + user.uid);
@@ -38,6 +43,7 @@ const Lobby = () => {
         console.log("Lobby useEfffect userState: ",userState);
         console.log("Lobby userid",user.uid);
         if(userState){
+        console.log("Lobby useEfffect userState IF: ",userState.data().state);
             if (userState.data().state == 'initial') {
                 history.push('/empieza-gratis');
             } else if(userState.data().state == 'consultancy') {
@@ -62,46 +68,46 @@ const Lobby = () => {
     
     const consultancyData = async () =>{
         setOpen(false);
-        // LobbyContainer
-        // let consultancy = await GetConsultancyRoomService(user.uid);
-        // setConsultancyRoom(consultancy);
-        // let adviser = await GetAviserService(consultancy.adviserid);
-        // setAdviser(adviser);
-        // if (!adviserPopUp && consultancyRoom.state) {
-        //     setAdviserPopUp(true);
-        //     alert(
-        //         "Se le acaba de asignar un asesor",
-        //         "Desea entrar a video llamada con el",
-        //         [
-        //             {
-        //                 text: "No",
-        //                 onPress: () => console.log("No, continue editing"),
-        //                 style: "cancel"
-        //             },
-        //             {
-        //                 text: "Si",
-        //                 onPress: () => videoCall(),
-        //             }
-        //         ],
-        //         { cancelable: false }
-        //     );
-        // }
+        let consultancy = await GetConsultancyRoomService(user.uid);
+        setConsultancyRoom(consultancy);
+        let adviser = await GetAviserService(consultancy.adviserid);
+        setAdviser(adviser);
+        if (!adviserPopUp && consultancyRoom.state) {
+            setAdviserPopUp(true);
+            alert(
+                "Se le acaba de asignar un asesor"//,
+                // "Desea entrar a video llamada con el",
+                // [
+                //     {
+                //         text: "No",
+                //         onPress: () => console.log("No, continue editing"),
+                //         style: "cancel"
+                //     },
+                //     {
+                //         text: "Si",
+                //         onPress: () => videoCall(),
+                //     }
+                // ],
+                // { cancelable: false }
+            );
+        }
     }
 
-    // In LobbyContainer
     const videoCall = async () => {
         // const resolution = await getResolution();
-        // history.push('VideoCall', {
-        //     sessionId: consultancyRoom.sessionid,
-        //     token: consultancyRoom.usertoken,
-        //     uid: user.uid,
-        //     type: 'consultancy',
-        //     employee: consultancyRoom.adviserid,
-            // resolution: resolution
-        // });
+        const resolution = '1280x720';
+        history.push('VideoCall', {
+            sessionId: consultancyRoom.sessionid,
+            token: consultancyRoom.usertoken,
+            uid: user.uid,
+            type: 'consultancy',
+            employee: consultancyRoom.adviserid,
+            resolution: resolution
+        });
     }
 
     return (
+        open ?
         <div style={{ marginTop: '5.5em', paddingLeft: '1em' }} >
             {
                 photoURL != null ?
@@ -124,6 +130,8 @@ const Lobby = () => {
             <p style={{ fontSize: '0.85em', fontWeight: 600, color: '#808080' }} >Disponibilidad todo el año.</p>
             <p style={{ fontSize: '1.5em', fontWeight: 'bold', color: '#009245' }} >¡Comencemos!</p>
         </div>
+        : // OR
+        <AdviserLobby />
     )
 }
 
