65816fb147fec5926fb3fdaf0ce1db38a644a6eb
CristianPedraza <cristian@mnatg.com>
2021-05-27 17:23:45 -0500

    WeDo Audit header

--- src/components/pages/QueHacemos/SectionB.jsx
+++ src/components/pages/QueHacemos/SectionB.jsx
@@ -5,16 +5,54 @@ import rocket from '../../../assets/images/WeDo/rocket.webp';
 // Styles
 import '../../../assets/styles/WeDo/SectionB.scss'
 import { Colors } from '../../../Theme/index';
-import { Grid, Box } from '@material-ui/core';
+import {
+  Grid,
+  Box,
+  makeStyles
+} from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  title: {
+    fontSize: '4.375em',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontFamily: 'PoppinsExtraBold',
+    width: '50%',
+    [theme.breakpoints.down('sm')]: {
+      width: '70%',
+      fontSize: '3.4375em'
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '100%'
+    }
+  },
+  rocket: {
+    position: 'absolute',
+    height: '24.25em',
+    width: '25.66875em',
+    marginLeft: '50%',
+    marginTop: '-14em',
+    [theme.breakpoints.down('sm')]: {
+      height: '18.25em',
+      width: '19.66875em',
+      marginTop: '-10.5em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      display: 'none'
+    }
+  }
+}));
 
-function SectionA() {
+const SectionA = () => {
+
+  const classes = useStyles();
+
   return (
     <Grid container  >
       <Grid item xs={12} >
-        <Box color={Colors.aquamarine} bgcolor={Colors.shadowGray} flexDirection='row' >
-          <h1 style={{ fontSize: '4.375em', textAlign: 'center', fontWeight: 'bold', fontFamily: 'PoppinsExtraBold' }} >AUDITORÍAS</h1>
-          <img src={rocket} alt="Rocket" />
+        <Box color={Colors.aquamarine} bgcolor={Colors.shadowGray} flexDirection='row' py={4.8125} >
+          <h1 className={classes.title} >AUDITORÍAS</h1>
+          <img src={rocket} alt="Rocket" className={classes.rocket} />
         </Box>
       </Grid>
     </Grid>
