9e8a2ea332ba94b37cbb335a23c3e74a9d8021a7
CristianPedraza <cristian@mnatg.com>
2021-05-27 17:32:20 -0500

    Update SectionB.jsx

--- src/components/pages/QueHacemos/SectionB.jsx
+++ src/components/pages/QueHacemos/SectionB.jsx
@@ -17,9 +17,11 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     fontWeight: 'bold',
     fontFamily: 'PoppinsExtraBold',
-    width: '50%',
+    width: '70%',
+    [theme.breakpoints.up('xl')]: {
+      width: '80%'
+    },
     [theme.breakpoints.down('sm')]: {
-      width: '70%',
       fontSize: '3.4375em'
     },
     [theme.breakpoints.down('xs')]: {
@@ -56,39 +58,6 @@ const SectionA = () => {
         </Box>
       </Grid>
     </Grid>
-    // <div className='Rectangle-quiene'>
-    //   <Grid className="ContainerGrid">
-    //     <div className="GridLeft">
-    //       <img className="imagenBQuienes" src={imagenB} alt="imagenB" /></div>
-    //     <div className="GridRight">
-    //       <p className='SecondTitle'>Proposito</p>
-    //       <p className='SecondText'>
-    //         Como corporación es brindar a nuestros clientes facilidad y calidad al momento de llevar a cabo nuestros servicios sin
-    //         la necesidad de tener que acudir a una oficina presencial, de esta manera buscamos no sólo ahorrar tiempo a nuestros clientes sino alinearnos a su beneficio.
-    //       <br></br>
-    //     Es además, traer la tecnología a las manos de nuestros clientes combinándolo con nuestros servicios profesionales,
-    //     sabemos que los tiempos van cambiando y hoy más que nunca estamos comprometidos con nuestros clientes para ayudarles
-    //      a resolver los problemas más complejos y también identificar las oportunidades. Siempre comprometidos a la calidad y
-    //      profesionalismo con el fin de  sobrepasar las expectativas de nuestros clientes.</p>
-    //     </div>
-    //   </Grid>
-    //   <Grid className="ContainerGridTablet">
-    //     <div className="GridsUp">
-    //       <img className="imagenBQuienes" src={imagenB} alt="imagenB" /></div>
-    //     <div className="GridUnder">
-    //       <p className='SecondTitle'>Proposito</p>
-    //       <p className='SecondText'>
-    //         Como corporación es brindar a nuestros clientes facilidad y calidad al momento de llevar a cabo nuestros servicios sin
-    //         la necesidad de tener que acudir a una oficina presencial, de esta manera buscamos no sólo ahorrar tiempo a nuestros clientes sino alinearnos a su beneficio.
-    //       <br></br>
-    //       <br></br>
-    //     Es además, traer la tecnología a las manos de nuestros clientes combinándolo con nuestros servicios profesionales,
-    //     sabemos que los tiempos van cambiando y hoy más que nunca estamos comprometidos con nuestros clientes para ayudarles
-    //      a resolver los problemas más complejos y también identificar las oportunidades. Siempre comprometidos a la calidad y
-    //      profesionalismo con el fin de  sobrepasar las expectativas de nuestros clientes.</p>
-    //     </div>
-    //   </Grid>
-    // </div>
   );
 }
 
