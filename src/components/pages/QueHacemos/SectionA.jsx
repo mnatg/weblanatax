3a8f42648d1a3bbf3e36d1064e7014d836bb2abe
Esteban1135730 <70926203+Esteban1135730@users.noreply.github.com>
2021-06-01 10:23:22 -0500

    section a que hacemos

--- src/components/pages/QueHacemos/SectionA.jsx
+++ src/components/pages/QueHacemos/SectionA.jsx
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom';
 import { Grid, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Balls from '../../../assets/images/WeDo/graphics-copy.webp';
+import BallTablet from '../../../assets/images/WeDo/ballstablet.webp';
+
 import plan1 from '../../../assets/images/WeDo/card-small-free.webp';
 import plan2 from '../../../assets/images/WeDo/card-small-basic.webp';
 import plan3 from '../../../assets/images/WeDo/card-small-premium.webp';
@@ -35,6 +37,9 @@ function SectionA() {
       fontWeight: 'bold',
       color: '#009245',
       fontSize: '6em',
+      [theme.breakpoints.down('xs')]: {
+        fontSize: '4em',
+      },
     },
     p: {
       color: '#009245',
@@ -55,8 +60,8 @@ function SectionA() {
         backgroundPosition: 'top',
         backgroundSize: 'contain',
         backgroundRepeat: 'no-repeat',
-      [theme.breakpoints.down('xs')]: {
-        backgroundImage: `url(${bgWeDoMovil})`,
+        [theme.breakpoints.down('xs')]: {
+          backgroundImage: `url(${bgWeDoMovil})`,
         },
 
       }
@@ -66,123 +71,141 @@ function SectionA() {
       marginLeft: '5em',
       marginTop: '5em',
       marginBottom: '5em',
-      [theme.breakpoints.down('md')]: {
+      [theme.breakpoints.down('sm')]: {
         marginLeft: 'auto',
         marginRight: 'auto',
         marginTop: '80%',
         textAlign: 'center',
-        [theme.breakpoints.down('xs')]: {
-          marginTop: '120%',
-        },
       },
-
+      [theme.breakpoints.down('xs')]: {
+        marginTop: '120%',
+      },
     },
     Balls: {
+    marginTop: '-18em',
+    width: '79%',
+    marginLeft: '10%',
+    marginRight: '10%',
+    [theme.breakpoints.down('lg')]: {
+      display: 'block'
+    },
+    [theme.breakpoints.down('sm')]: {
+      display:'none',
+    },
+    [theme.breakpoints.down('xs')]: {
+      display: 'none',
+    },
+  },
+    BallsMovil: {
+    [theme.breakpoints.down('lg')]: {
+      display: 'none'
+    },
+    [theme.breakpoints.down('sm')]: {
+      display:'block',
       marginTop: '-18em',
-      width: '79%',
-      marginLeft: '10%',
-      marginRight: '10%',
-      [theme.breakpoints.down('xs')]: {
-        display:'none',
-      },
+      width: '95%',
+      marginLeft: '2.5%',
+      marginRight: '2.5%',
     },
+  },
     plan: {
+    marginTop: '-7%',
+    width: '90%',
+    marginBottom: '2em',
+    [theme.breakpoints.down('lg')]: {
+      width: '95%',
+      marginBottom: '2em',
       marginTop: '-7%',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '80%',
+      marginTop: '0',
+      marginLeft: '10%',
+      marginRight: '10%'
+    },
+    [theme.breakpoints.down('xs')]: {
       width: '90%',
       marginBottom: '2em',
-      [theme.breakpoints.down('lg')]: {
-        width: '95%',
-        marginBottom: '2em',
-        marginTop: '-7%',
-      },
-      [theme.breakpoints.down('md')]: {
-        width: '80%',
-        marginTop: '0',
-        marginLeft: '10%',
-        marginRight: '10%'
-      },
-      [theme.breakpoints.down('xs')]: {
-        width: '90%',
-        marginBottom: '2em',
-      },
     },
+  },
     plan2: {
+    width: '90%',
+    marginBottom: '2em',
+    [theme.breakpoints.down('lg')]: {
+      width: '95%',
+      marginBottom: '2em',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '80%',
+      marginTop: '0',
+      marginLeft: '10%',
+      marginRight: '10%'
+    },
+    [theme.breakpoints.down('xs')]: {
       width: '90%',
       marginBottom: '2em',
-      [theme.breakpoints.down('lg')]: {
-        width: '95%',
-        marginBottom: '2em',
-      },
-      [theme.breakpoints.down('md')]: {
-        width: '80%',
-        marginTop: '0',
-        marginLeft: '10%',
-        marginRight: '10%'
-      },
-      [theme.breakpoints.down('xs')]: {
-        width: '90%',
-        marginBottom: '2em',
-      },
     },
+  },
 
     destop: {
-      [theme.breakpoints.down('md')]: {
-        display: 'none'
-      },
-      [theme.breakpoints.down('xs')]: {
-        display: 'none'
-      },
-    }
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    },
+    [theme.breakpoints.down('xs')]: {
+      display: 'none'
+    },
+  }
 
   }));
-  const classes = useStyles();
+const classes = useStyles();
 
-  return (
+return (
 
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Paper className={classes.header}><p className={classes.title} >TAXES</p></Paper>
-      </Grid>
-      <Grid item xs={12} className={classes.content}>
-        <img src={Balls} alt="Balls" className={classes.Balls} />
-        <Grid item lg={3} md={9} xs={9} className={classes.contentTex}>
-          <Paper className={classes.paper}><p className={classes.p} >Hasta para los servicios básicos necesitamos tener al día nuestras cuentas de taxes (impuestos)
-          y debido a que en nuestros países latinos no es un tema relevante, chocamos cuando ya viviendo
-          en EEUU las cosas con los impuestos cambian, ya que como persona natural  debes declarar tanto tus
-          gastos y ganancias absolutamente todos los años y como sabemos que tienes otras cosas por las cuales
-          ocuparse claramente los taxes quedan para lo último y muchas veces se te olvida ¡Y no queremos que eso
+  <Grid container spacing={3}>
+    <Grid item xs={12}>
+      <Paper className={classes.header}><p className={classes.title} >TAXES</p></Paper>
+    </Grid>
+    <Grid item xs={12} className={classes.content}>
+      <img src={Balls} alt="Balls" className={classes.Balls} />
+      <img src={BallTablet} alt="Balls" className={classes.BallsMovil} />
+      <Grid item lg={5} md={9} xs={9} className={classes.contentTex}>
+        <Paper className={classes.paper}><p className={classes.p} >Hasta para los servicios básicos necesitamos tener al día nuestras cuentas de taxes (impuestos)
+        y debido a que en nuestros países latinos no es un tema relevante, chocamos cuando ya viviendo
+        en EEUU las cosas con los impuestos cambian, ya que como persona natural  debes declarar tanto tus
+        gastos y ganancias absolutamente todos los años y como sabemos que tienes otras cosas por las cuales
+        ocuparse claramente los taxes quedan para lo último y muchas veces se te olvida ¡Y no queremos que eso
                                                                          te ocurra!. Por eso Lana Tax está siempre contigo desde tu celular, déjalo en nuestras manos.<br></br>
-            <br></br>
+          <br></br>
                                                                         Debido a que cada persona es diferente, Lana tax ha creado varios planes para cada una de ellas, ya que no
                                                                          es lo mismo realizar planes para una persona que trabaja para una empresa, que realizar taxes para una
                                                                          persona quien es propietaria de la empresa. A continuación te mostraremos el plan que hemos diseñado para
                                                                          cada tipo de persona.</p></Paper>
-        </Grid>
       </Grid>
-      <Grid container direction="row" justify="center" alignItems="center" >
-        <Grid item xs={9} md={9} lg={3}>
-          <img src={plan1} alt="card-small-free" className={classes.plan} />
-        </Grid>
-        <Grid item xs={9} md={6} lg={3}>
-          <img src={plan2} alt="card-small-basic" className={classes.plan} />
-        </Grid>
-        <Grid item xs={9} md={6} lg={3} >
-          <img src={plan3} alt="card-small-premium" className={classes.plan} />
-        </Grid>
-        <hr className={classes.destop}></hr>
-        <Grid item xs={9} md={6} lg={3} >
-          <img src={plan4} alt="card-small-deluxe" className={classes.plan2} />
-        </Grid>
-        <Grid item xs={9} md={6} lg={3}>
-          <img src={plan5} alt="card-small-deluxe-copy" className={classes.plan2} />
-        </Grid>
+    </Grid>
+    <Grid container direction="row" justify="center" alignItems="center" >
+      <Grid item xs={9} sm={9} lg={3}>
+        <img src={plan1} alt="card-small-free" className={classes.plan} />
       </Grid>
-    </Grid >
+      <Grid item xs={9} sm={6} lg={3}>
+        <img src={plan2} alt="card-small-basic" className={classes.plan} />
+      </Grid>
+      <Grid item xs={9} sm={6} lg={3} >
+        <img src={plan3} alt="card-small-premium" className={classes.plan} />
+      </Grid>
+      <hr className={classes.destop}></hr>
+      <Grid item xs={9} sm={6} lg={3} >
+        <img src={plan4} alt="card-small-deluxe" className={classes.plan2} />
+      </Grid>
+      <Grid item xs={9} sm={6} lg={3}>
+        <img src={plan5} alt="card-small-deluxe-copy" className={classes.plan2} />
+      </Grid>
+    </Grid>
+  </Grid >
 
 
 
 
-  );
+);
 }
 
 export default SectionA;
