faa2a9fb4a92efb067d48d2014e02d70182d9f62
CristianPedraza <79148937+CristianPedraza@users.noreply.github.com>
2021-07-12 15:12:05 -0500

    WeDo

--- src/components/pages/QueHacemos/Bottom.jsx
+++ src/components/pages/QueHacemos/Bottom.jsx
@@ -1,281 +1,102 @@
 // React
 import React from 'react';
 // Components
-import { Grid } from '@material-ui/core';
+import { Grid, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 // Assets
 import BottomBackground from '../../../assets/images/WeDo/BottomBackground.webp'
-import BottomBackgroundTablet from '../../../assets/images/WeDo/ButtonBackgroundTablet.png'
-import BottomBackgroundMobile from '../../../assets/images/WeDo/ButtonBackgroundMobile.png'
-import Cohete from '../../../assets/images/WeDo/cohete.png'
+import BottomBackgroundTablet from '../../../assets/images/WeDo/BottonBackgroundTablet.webp'
+import BottomBackgroundMobile from '../../../assets/images/WeDo/BottonBackgroundMobile.webp'
+import rocket from '../../../assets/images/WeDo/rocket.webp'
 
 function Bottom() {
   const useStyles = makeStyles((theme) => ({
-    colorOne:{
-      [theme.breakpoints.down('xl')]: {
-        backgroundColor: 'transparent', 
-      },
-      [theme.breakpoints.down('md')]: {
-        backgroundColor: 'rgba(0,146,69,0.8)', 
-    
-      },
-      [theme.breakpoints.down('xs')]: {
-     
-      },
-    
-    },
-    colorTwo:{
-      [theme.breakpoints.down('xl')]: {
-        backgroundColor: 'rgba(0,146,69,0.8)', 
-      },
-      [theme.breakpoints.down('md')]: {
-        backgroundColor: 'transparent', 
-    
-      },
-      [theme.breakpoints.down('xs')]: {
-        
-      },
-    
-    },
-    colorThree:{
-      [theme.breakpoints.down('xl')]: {
-        backgroundColor: 'rgba(0,146,69,0.8)',   
-      },
-      [theme.breakpoints.down('md')]: {
-        
-        backgroundColor: 'rgba(0,146,69,0.8)', 
-      },
-      [theme.breakpoints.down('xs')]: {
-        
-      },
-    
-    },
-    colorFour:{
-      [theme.breakpoints.down('xl')]: {
-        backgroundColor: 'transparent',  
-      },
-      [theme.breakpoints.down('md')]: {
-        
-        backgroundColor: 'transparent', 
-      },
-      [theme.breakpoints.down('xs')]: {
-        
-      },
-    
-    },
-    textOne: {
-      width: '21em',
-      fontFamily: 'Poppins',
-      fontWeight: 'normal',
-      color: '#a6a6a6',
-      fontSize: '1em',
-      lineHeight: '1.5em',
-      marginBlock: '5em',
-      marginInline: 'auto',
-      [theme.breakpoints.down('lg')]: {
-    
-
-      }, [theme.breakpoints.down('md')]: {
-        color: '#ffffff',
-        textAlign:'center',
-        marginTop: '11em',
-
-      }, [theme.breakpoints.down('xs')]: {
-    
-        color: '#ffffff',
-        textAlign:'center',
-        marginTop: '5em',
-      }
-    },
-    textTwo: {
-      width: '21em',
-      fontFamily: 'Poppins',
-      fontWeight: 'normal',
-      color: '#ffffff',
-      fontSize: '1em',
-      lineHeight: '1.5em',
-      marginBlock: '5em',
-      marginInline: 'auto',
-      
-      [theme.breakpoints.down('lg')]: {
-     
-
-      }, [theme.breakpoints.down('md')]: {
-        color: '#a6a6a6',
-        textAlign:'center',
-        marginTop: '10em',
-
-      }, [theme.breakpoints.down('xs')]: {
-        color: '#a6a6a6',
-        textAlign:'center',
-        marginTop: '10em',
-    
-      }
-    },
-    textThree: {
-      width: '21em',
-      fontFamily: 'Poppins',
-      fontWeight: 'normal',
-      color: '#ffffff',
-      fontSize: '1em',
-      lineHeight: '1.5em',
-      marginBlock: '11em',
-      marginInline: 'auto',
-      [theme.breakpoints.down('lg')]: {
-      
-      }, [theme.breakpoints.down('md')]: {
-        marginTop: '15em',
-        textAlign:'center',
-      }, [theme.breakpoints.down('xs')]: {
-        marginTop: '15em',
-        textAlign:'center',
-      }
-    },
-    textFour: {
-      width: '21em',
-      fontFamily: 'Poppins',
-      fontWeight: 'normal',
-      color: '#a6a6a6',
-      fontSize: '1em',
-      lineHeight: '1.5em',
-      marginTop: '11em',
-      marginInline: 'auto',
-      [theme.breakpoints.down('lg')]: {
-      
-
-      }, [theme.breakpoints.down('md')]: {
-        marginTop: '15em',
-        textAlign:'center',
-      }, [theme.breakpoints.down('xs')]: {
-        marginTop: '15em',
-        textAlign:'center',
-
-      }
-    },
-    bottomDescription: {
-      width: '42em',
-      heigth: '9.5em',
-      fontFamily: 'Poppins',
-      color: '#d9e021',
-      fontSize: '2em',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      marginBlock: '2em',
-      textAlign: 'center',
-      fontWeight: '600',
-      [theme.breakpoints.down('lg')]: {
-        marginTop: '1em',
-        width: '38em',
-
-      }, [theme.breakpoints.down('md')]: {
-        fontSize: '1.7em',
-        width: '15em',
-        width: '27em',
-        marginTop: '8em',
-
-      }, [theme.breakpoints.down('xs')]: {
-        fontSize: '1em',
-        width: '20em',
-      }
-
-    },
-    content: {
+    container: {
       backgroundImage: `url(${BottomBackground})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       maxWidth: '1600px',
-      border: '0',
       marginInline: 'auto',
-      [theme.breakpoints.down('md')]: {
+      justifyContent: 'center',
+      [theme.breakpoints.down('sm')]: {
         backgroundImage: `url(${BottomBackgroundTablet})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        height: '120em',
-        marginBottom: '14em',
-        marginTop: '0em',
-      },
-      [theme.breakpoints.down('xs')]: {
-        backgroundImage: `url(${BottomBackgroundMobile})`,
-        backgroundSize: 'contain',
-        backgroundPosition: 'inherit',
-        marginBottom: '-10em'
       },
     },
-    cohete: {
-      backgroundImage: `url(${Cohete})`,
-      backgroundSize: 'contain',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      marginTop: '0em',
-      maxWidth: '1600px',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      position: 'relative',
-   
-      [theme.breakpoints.down('lg')]: {
-
-      },
-      [theme.breakpoints.down('md')]: {
-        marginTop: '0em',
-      },
-      [theme.breakpoints.down('xs')]: {
-     
+    primaryChange: {
+      backgroundColor: 'rgba(0,146,69,0.8)',
+      color: 'white',
+      [theme.breakpoints.down('sm')]: {
+        backgroundColor: 'transparent',
+        color: '#a6a6a6',
       },
     },
-    cohetep: {
-     
-      marginTop: '0em',
-      maxWidth: '1600px',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      position: 'relative',
-   
-      [theme.breakpoints.down('lg')]: {
-
-      },
-      [theme.breakpoints.down('md')]: {
-    
-        marginTop: '0em',
-      },
-      [theme.breakpoints.down('xs')]: {
-     
+    secondaryChange: {
+      backgroundColor: 'transparent',
+      color: '#a6a6a6',
+      [theme.breakpoints.down('sm')]: {
+        backgroundColor: 'rgba(0,146,69,0.8)',
+        color: 'white',
       },
     },
-    container: {
+    primary: {
+      backgroundColor: 'rgba(0,146,69,0.8)',
+      color: 'white',
+    },
+    secondary: {
+      backgroundColor: 'transparent',
+      color: '#a6a6a6',
+    },
+    texts: {
+      [theme.breakpoints.down('sm')]: {
+        textAlign: 'center',
+      },
+    },
+    rocket: {
+      position: 'absolute',
+      marginTop: '30em',
+      [theme.breakpoints.down('sm')]: {
+        marginTop: '75em',
+      },
+    },
+    finalText: {
+      color: '#d9e021',
+      fontSize: '1.5em',
+      textAlign: 'center',
+      marginTop: '2em',
       marginInline: 'auto',
-      paddingBlock: '10%'
+      width: '65%',
     }
   }));
 
+  const contentBox = (text) => (<Box px={{sm: 33, md: 22}} py={35}  className={classes.texts} >
+    <p >{text}</p>
+  </Box>)
+
   const classes = useStyles();
 
-  return (
-    <div className='pngcontainer1'>
-      <img src={Cohete} ></img>
-      <div className='circleOne hideCircles'></div>
-      <div className='circleTwo hideCircles'></div>
-      <div className='circleThree hideCircles'></div>
-      <div className='circleFour hideCircles'></div>
-      <Grid container className={classes.root} className={classes.content}>
-        <Grid item xs={12} lg={6} xl={6} className={[classes.colorOne, classes.container]}>
-          <p className={classes.textOne}>Lamentablemente, nuestra comunidad ha sido víctima de personas inescrupulosas quienes sin estar habilitadas han practicado ante el IRS y los Departamentos de Impuestos Estatales, colocando a personas y negocios de nuestra comunidad en serios problemas tributarios.
-</p></Grid>
-        <Grid item xs={12} lg={6} xl={6} className={[classes.colorTwo, classes.container]}>
-          <p className={classes.textTwo}>Lanatax le brinda a nuestra comunidad EN SU IDIOMA un servicio profesional, personalizado, seguro y de calidad. Nosotros nos encargamos de hacerte fácil y seguro tu camino de impuestos (taxes).</p>
-        </Grid>
 
-        <Grid item xs={12} lg={6} xl={6} className={[classes.colorThree, classes.container]}>
-          <p className={classes.textThree}>Adiós a las malas experiencias y si quieres iniciarte en impuestos somos la compañía correcta, Lanatax no te abandona desde el principio hasta el final de tu proceso de impuestos.
-</p></Grid>
-        <Grid item xs={12} lg={6} xl={6} className={[classes.colorFour, classes.container]}>
-          <p className={classes.textFour}>¡No queremos que te sientas inseguro en temas tributarios, déjalos en nuestras manos!. Por eso Lanatax está siempre contigo desde tu celular, cerca de ti.
-</p>
-        </Grid>
-
+  return (<>
+    <Grid item xs={12} container className={classes.container} >
+      <Grid item xs={12} md={6} className={classes.secondaryChange} >
+        {contentBox('Lamentablemente, nuestra comunidad ha sido víctima de personas inescrupulosas quienes sin estar habilitadas han practicado ante el IRS y los Departamentos de Impuestos Estatales, colocando a personas y negocios de nuestra comunidad en serios problemas tributarios.')}
       </Grid>
-      <p className={classes.bottomDescription}>Lanatax ha creado planes que se ajustan a tu realidad tributaria, pues no es lo mismo proceso para un contratista, inversionista o un empleado. A continuación te mostraremos el plan que hemos diseñado para cada tipo de situación tributaria.
-</p>
-    </div>
+      <Grid item xs={12} md={6} className={classes.primaryChange} >
+        {contentBox('Lanatax le brinda a nuestra comunidad EN SU IDIOMA un servicio profesional, personalizado, seguro y de calidad. Nosotros nos encargamos de hacerte fácil y seguro tu camino de impuestos (taxes).')}
+      </Grid>
+      <Grid item xs={12} className={classes.rocket} >
+        <Box >
+          <img src={rocket} alt="Rocket" />
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={6} className={classes.primary} >
+        {contentBox('Adiós a las malas experiencias y si quieres iniciarte en impuestos somos la compañía correcta, Lanatax no te abandona desde el principio hasta el final de tu proceso de impuestos.')}
+      </Grid>
+      <Grid item xs={12} md={6} className={classes.secondary} >
+        {contentBox('¡No queremos que te sientas inseguro en temas tributarios, déjalos en nuestras manos!. Por eso Lanatax está siempre contigo desde tu celular, cerca de ti.')}
+      </Grid>
+    </Grid>
+    <p className={classes.finalText} >Lanatax ha creado planes que se ajustan a tu realidad tributaria, pues no es lo mismo proceso para un contratista, inversionista o un empleado. A continuación te mostraremos el plan que hemos diseñado para cada tipo de situación tributaria.</p>
+  </>
   );
 }
 
--- src/components/pages/QueHacemos/Header.jsx
+++ src/components/pages/QueHacemos/Header.jsx
@@ -7,88 +7,88 @@ import tabletHeader from '../../../assets/images/WeDo/tabletHeader.webp'
 import mobileHeader from '../../../assets/images/WeDo/mobileHeader.webp'
 
 function Header() {
-const useStyles = makeStyles((theme) => ({
-  title: {
-    width: '5em',
-    heigth: 'auto',
-    fontFamily: 'PoppinsExtraBold',
-    fontWeight: 'bold',
-    color: '#009245',
-    fontSize: '5em',
-    lineHeight: '1em',
-    marginTop: '2.53125em',
-    marginLeft:'4em',
-    marginBottom:'0.5em',
-    [theme.breakpoints.up('xl')]: {
-      marginTop: '3.625em'
-    },
-    [theme.breakpoints.down('lg')]: {
-      fontSize: '4em',
-    },[theme.breakpoints.down('sm')]: {
-      width: '100%',
-      marginInline:'auto',
-      marginTop: '14.3em',
-      marginBottom: '1.5em',
-      textAlign:'center',
-      fontSize: '5.5em',
-    },[theme.breakpoints.down('xs')]: {
-      marginTop: '22em',
-      marginBottom: '0.8em',
-      fontSize: '3.5em',
+  const useStyles = makeStyles((theme) => ({
+    title: {
       width: '5em',
-    }
-  },
+      heigth: 'auto',
+      fontFamily: 'PoppinsExtraBold',
+      fontWeight: 'bold',
+      color: '#009245',
+      fontSize: '5em',
+      lineHeight: '1em',
+      marginTop: '2.53125em',
+      marginLeft: '4em',
+      marginBottom: '0.5em',
+      [theme.breakpoints.up('xl')]: {
+        marginTop: '3.625em'
+      },
+      [theme.breakpoints.down('lg')]: {
+        fontSize: '4em',
+      }, [theme.breakpoints.down('sm')]: {
+        width: '100%',
+        marginInline: 'auto',
+        marginTop: '14.3em',
+        marginBottom: '1.5em',
+        textAlign: 'center',
+        fontSize: '5.5em',
+      }, [theme.breakpoints.down('xs')]: {
+        marginTop: '22em',
+        marginBottom: '0.8em',
+        fontSize: '3.5em',
+        width: '5em',
+      }
+    },
     titleDescripcion: {
       width: '20em',
       heigth: '9.5em',
       fontFamily: 'Poppins',
       color: '#2f281e',
       fontSize: '1em',
-      marginLeft:'20em',
-      marginBottom:'12.5em',
+      marginLeft: '20em',
+      marginBottom: '12.5em',
       [theme.breakpoints.down('lg')]: {
-        marginLeft:'16em',
-      },[theme.breakpoints.down('sm')]: {
-        marginInline:'auto',
-        textAlign:'center',
-        width:'75%',
-        color:'#a6a6a6',
+        marginLeft: '16em',
+      }, [theme.breakpoints.down('sm')]: {
+        marginInline: 'auto',
+        textAlign: 'center',
+        width: '75%',
+        color: '#a6a6a6',
         fontSize: '1.3em',
-      },[theme.breakpoints.down('xs')]: {
-        width:'80%',
+      }, [theme.breakpoints.down('xs')]: {
+        width: '80%',
         fontSize: '1.2em',
       }
     },
-   headerDescription: {
+    headerDescription: {
       width: '42em',
       heigth: '9.5em',
       fontFamily: 'Poppins',
       color: '#009245',
       fontSize: '2em',
-      marginInline:'auto',
-      marginBlock:'3em',
-      textAlign:'center',
-      fontWeight:'600',
+      marginInline: 'auto',
+      marginBlock: '3em',
+      textAlign: 'center',
+      fontWeight: '600',
       [theme.breakpoints.down('lg')]: {
-        marginBlock:'2em',
+        marginBlock: '2em',
         width: '33em',
-      },[theme.breakpoints.down('sm')]: {
+      }, [theme.breakpoints.down('sm')]: {
         fontSize: '1.7em',
         width: '85%',
-        marginTop:'12em',
-        marginBottom:'5em',
-      },[theme.breakpoints.down('xs')]: {
+        marginTop: '12em',
+        marginBottom: '5em',
+      }, [theme.breakpoints.down('xs')]: {
         fontSize: '1.2em',
         width: '80%',
-        marginTop:'17em',
+        marginTop: '17em',
       }
     },
     content: {
       backgroundImage: `url(${desktopHeader})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
-      maxWidth:'1600px',
-      marginInline:'auto',
+      maxWidth: '1600px',
+      marginInline: 'auto',
       [theme.breakpoints.down('sm')]: {
         backgroundImage: `url(${tabletHeader})`,
         height: '86.8em',
@@ -97,21 +97,21 @@ const useStyles = makeStyles((theme) => ({
         backgroundImage: `url(${mobileHeader})`,
       },
     },
-}));
+  }));
 
-const classes = useStyles();
+  const classes = useStyles();
 
-const textTitle = 'Entender el sistema de impuestos de los Estados Unidos de América puede resultar complicado y en la mayoría de los casos desconocemos que la obligación de rendir sus impuestos también representa beneficios para los  contribuyentes activos.';
-const textBody = 'Nuestra comunidad latina merece tener el mejor servicio en materia de impuestos y de asesoría de negocios en su idioma. Lanatax  garantiza a nuestra comunidad la asesoría completa en cuestión tributaria para usted y para su negocio.'
+  const textTitle = 'Entender el sistema de impuestos de los Estados Unidos de América puede resultar complicado y en la mayoría de los casos desconocemos que la obligación de rendir sus impuestos también representa beneficios para los  contribuyentes activos.';
+  const textBody = 'Nuestra comunidad latina merece tener el mejor servicio en materia de impuestos y de asesoría de negocios en su idioma. Lanatax  garantiza a nuestra comunidad la asesoría completa en cuestión tributaria para usted y para su negocio.'
 
   return (
-      <Grid container >
-        <Grid item xs={12} className={classes.content} > 
-            <p className={classes.title}>QUÉ HACEMOS</p>
-            <p className={classes.titleDescripcion}>{textTitle}</p>
-        </Grid>
-        <p className={classes.headerDescription}>{textBody}</p>
+    <Grid container >
+      <Grid item xs={12} className={classes.content} >
+        <p className={classes.title}>QUÉ HACEMOS</p>
+        <p className={classes.titleDescripcion}>{textTitle}</p>
       </Grid>
+      <p className={classes.headerDescription}>{textBody}</p>
+    </Grid>
   );
 }
 
